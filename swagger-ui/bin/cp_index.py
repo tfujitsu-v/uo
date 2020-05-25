ad298f391d62b2477fd778b85fdb5a53d6c51d24
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-25 14:14:38 +0900

    script

--- swagger-ui/bin/cp_index.py
+++ swagger-ui/bin/cp_index.py
@@ -1,9 +1,14 @@
 import glob, os, shutil
+import subprocess
 
-dir = os.getcwd()
-swagger_ui_html = dir + "/swagger-ui/dist/swaggerUI.html"
-print(swagger_ui_html)
-ymls = glob.glob(dir + "/swagger-ui/dist/api/*.yaml", recursive=True)
-for yml in ymls:
-    shutil.copyfile(swagger_ui_html,yml.replace("yaml", "html"))
+basedir = os.getcwd()
+swagger_ui_html = basedir + "/swagger-ui/dist/swaggerUI.html"
+dir = basedir + "/swagger-ui/dist/api/"
+for root, dirs, files in os.walk(dir):
+    for fname in files:
+        if fname.find("yaml"):
+            _root, ext = os.path.splitext(fname)
+            if ext == ".yaml":
+                target_file = root + "/" + fname
+                shutil.copyfile(swagger_ui_html,target_file.replace("yaml", "html"))
 
