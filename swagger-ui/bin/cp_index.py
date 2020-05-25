94c169e6942065ccb07b030f3cb654f18da2719e
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-25 11:19:52 +0900

    cp command

--- swagger-ui/bin/cp_index.py
+++ swagger-ui/bin/cp_index.py
@@ -1,1 +1,10 @@
-print("aaaaaaa")
+import glob, os, shutil
+
+dir = os.getcwd()
+swagger_ui_html = dir + "/swagger-ui/dist/swaggerUI.html"
+print(swagger_ui_html)
+ymls = glob.glob(dir + "/swagger-ui/dist/api/*.yaml")
+for yml in ymls:
+    shutil.copyfile(swagger_ui_html,yml.replace("yaml", "html"))
+    print(yml)
+
