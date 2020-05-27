f332aca483d33b0956f5d0cb0def56cf84538eed
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-27 16:41:20 +0900

    wip

--- bin/cp_index.py
+++ bin/cp_index.py
@@ -1,8 +1,16 @@
-import glob, os, shutil
+import glob, os, shutil, sys
 import subprocess
 
 basedir = os.getcwd()
 swagger_ui_html = basedir + "/swaggerUI.html"
+
+# devの場合は、noindexを付与
+if len(sys.argv) > 1 and sys.argv[1] == 'dev':
+    with open(swagger_ui_html,　"w"　,encoding="utf-8") as file:
+        filedata=file.read()
+        filedata=filedata.replace('<head>','<head><meta name="robots" content="nofollow" />')
+        file.write(filedata)
+
 dir = basedir + "/api/"
 for root, dirs, files in os.walk(dir):
     for fname in files:
