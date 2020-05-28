ab9ab60bab7835be4dedde1b1e2b9877c6286893
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-28 09:59:15 +0900

    wip

--- bin/cp_index.py
+++ bin/cp_index.py
@@ -1,16 +1,20 @@
+# -*- Coding: utf-8 -*-
+
 import glob, os, shutil, sys
 import subprocess
 
 basedir = os.getcwd()
+files = ["index.html", "swaggerUI.html"]
 swagger_ui_html = basedir + "/swaggerUI.html"
 
 # Added NOINDEX in Staging
 if len(sys.argv) > 1 and sys.argv[1] == 'dev':
-    with open(swagger_ui_html, "r") as file:
-        data_lines=file.read()
-    data_lines = data_lines.replace('<head>', '<head><meta name="robots" content="noindex, nofollow" />')
-    with open(swagger_ui_html, mode="w") as f:
-        f.write(data_lines)
+    for file in files:
+        with open(swagger_ui_html, "r") as file:
+            data_lines=file.read()
+        data_lines = data_lines.replace('<head>', '<head><meta name="robots" content="noindex, nofollow" />')
+        with open(swagger_ui_html, mode="w") as f:
+            f.write(data_lines)
 
 dir = basedir + "/api/"
 for root, dirs, files in os.walk(dir):
