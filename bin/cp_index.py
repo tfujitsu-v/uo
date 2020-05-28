dfc20c665f4f10640e8dd7979273650c0b5ac747
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-28 14:18:00 +0900

    w

--- bin/cp_index.py
+++ bin/cp_index.py
@@ -11,7 +11,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'dev':
     with open(swagger_ui_html, "r") as file:
         data_lines=file.read()
     data_lines = data_lines.replace('<head>', '<head><meta name="robots" content="noindex, nofollow" />')
-    data_lines = data_lines.replace('<title>', u'<title>ご確認ください。')
+    data_lines = data_lines.replace('<title>', '<title>ご確認ください。')
     with open(swagger_ui_html, mode="w") as f:
         f.write(data_lines)
 
