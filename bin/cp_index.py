30f24731a7f8d0a8db93bf4dbf208759f91d998f
tfujitsu <tfujitsu@tfujitsunoMacBook-Pro.local>
2020-05-29 15:47:57 +0900

    pyyaml

--- bin/cp_index.py
+++ bin/cp_index.py
@@ -21,5 +21,6 @@ for root, dirs, files in os.walk(dir):
             if ext == ".yaml":
                 target_file = root + "/" + fname
                 yml = yaml.load(target_file)
+                print(yml)
                 shutil.copyfile(swagger_ui_html,target_file.replace("yaml", "html"))
 
